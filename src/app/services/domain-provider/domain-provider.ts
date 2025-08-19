695e873aac560e4adaa45a9d023c2c0057d82372
MrDvD <ma.labin@yandex.ru>
2025-08-19 20:15:00 +0500

    fix: change external api url

--- src/app/services/domain-provider/domain-provider.ts
+++ src/app/services/domain-provider/domain-provider.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DomainProvider {
   public getApiDomain(): string {
-    return 'https://8d414153df7c.ngrok-free.app';
+    return 'https://fawn-logical-adequately.ngrok-free.app';
   }
 }
