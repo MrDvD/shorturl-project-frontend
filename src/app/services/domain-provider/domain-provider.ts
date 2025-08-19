7fa866688ebafd68ea5f7095241b1d3bd39cac13
MrDvD <ma.labin@yandex.ru>
2025-08-19 22:29:22 +0500

    feat: change service provider

--- src/app/services/domain-provider/domain-provider.ts
+++ src/app/services/domain-provider/domain-provider.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DomainProvider {
   public getApiDomain(): string {
-    return 'https://fawn-logical-adequately.ngrok-free.app';
+    return 'https://my.cloudpub.ru';
   }
 }
